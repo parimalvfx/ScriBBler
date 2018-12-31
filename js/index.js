898ca3a07288d6693c1ea34e299fbd9e9e5f54ba
shashankjndl20 <43314491+shashankjndl20@users.noreply.github.com>
2018-12-31 22:39:34 +0530

    HomepageUpdated

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-function toggleModal1(modalID) {
-
-    var modal = document.getElementById(modalID);
-    if (modal.style.display == '') {
-        modal.style.display = 'block';
-    }
- else {
-        modal.style.display = '';
-    }
-}
-
 document.getElementById("allposts").addEventListener("click", myFunction);
 
 function myFunction() {
